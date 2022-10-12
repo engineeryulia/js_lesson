46d3a26b7f877eecf4300deba8cb14be15f6006f
Yulia <you@example.com>
2022-10-11 18:53:57 -0600

    object

---
+++ object.js
@@ -0,0 +1,14 @@
+// let obj ={
+//   firstName: 'Sofia',
+//   lastName:'I.',
+//   age: 35
+
+// }
+
+
+//================Example
+// let userInfo = {
+//   name: 'Anna',
+//   age: 30,
+//   adress: 
+// }
