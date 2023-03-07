0146627213d1b9ee68a6df1f0e25f9cf4d775d7a
Yulia Lopatko <engineeryulia@gmail.com>
2023-03-06 18:33:32 -0700

    add coffeshop

--- 6.js/2_ifElse.js
+++ 6.js/2_ifElse.js
@@ -21,3 +21,37 @@ else if(pass != true && login){
 else {
   console.log('Please contact suport')
 }
+//Svetlana
+let kidsTicket, adultsTicket, age, price, isVeterans, seniors;
+kidsTicket = "Hello Kitty";
+adultTicket = "Ray";
+age = prompt("How old are you?");
+let totalPrice;
+let guestNum = prompt("How many tickets do you need?")
+price = 10;
+
+if(age >= 18){
+  totalPrice = guestNum * price;
+  alert(`Welcome to ${adultTicket} movie`);
+  console.log(`Ticket price for ${guestNum} tickets for  ${adultTicket} movie will be $ ${totalPrice}`);
+ }else{
+  totalPrice = guestNum * price / 2;
+  console.log(`Ticket price for ${guestNum} tickets for  ${kidsTicket} movie will be $ ${totalPrice}`);
+   alert(`Go to ${kidsTicket}`);
+ }
+   //45min
+
+//Nalya
+/* «Улитка ползёт вверх по стене высотой 5 метров. Каждый день она проползает вверх на 3 метра, а каждую ночь съезжает вниз на 2 метра. За сколько дней она доползёт до вершины стены.»
+*/   
+let a = 3
+
+
+
+/*Напишите программу по согласованию отпуска в зависимости от месяца. Если месяц “July” или “August”, то программа должна показать - ‘approved’, если другие месяцы, то - “denied”.
+ */
+
+
+/* Напишите простенький калькулятор (*, /, +, -). Проверьте ваш код на охват исключений для арифметических операций.
+У вас должны быть две переменные для исходных чисел и одна для оператора. В зависимости от оператора, должно происходить то или иное арифметическое действие и выводиться результат в консоль.
+*/
---
+++ 6.js/coffeshop.js
@@ -0,0 +1,23 @@
+let cup = 'cup '
+let water = 'cold water'
+let price = 5
+let milk = 'milk'
+let coffee = 'Latte'
+//steps for coffee recipe
+let order = `You choosed a ${cup}`
+let step1 = `1. Take a ${cup}`
+let step2 = `2. Put 1 spoon of ${coffee} in it`
+let step3 = `3. Pour ${water} in a cup with cofee`
+let step4 = `4. Pour milk in a cup of ${coffee}`
+let step5 = `5. Stir ${coffee} in a ${cup} with a spoon`
+let orderPrice = `The order price is ${price}$`
+let result = `Enjoy your coffee!`
+
+
+
+document.write (
+                order + '<br>' + '<br>',
+                step1 + '<br>' + step2 + '<br>',
+                step3 + '<br>' + step4 + '<br>',
+                step5 + '<br>' + result)
+ 
